af83a61498ecf6f0920cdeaa66106576629f9c62
Nathan <seu-email>
2024-10-29 16:36:54 -0300

    atualizacao

--- app/routes/router.js
+++ app/routes/router.js
@@ -18,6 +18,49 @@ router.get("/calendario", function (req, res) {
     res.render("pages/cadlog")
    });
    
+   router.get("/Brocolis", function (req, res) {
+    res.render("pages/Brocolis")
+   });
+
+   router.get("/ameixa", function (req, res) {
+    res.render("pages/ameixa")
+   });
+
+   router.get("/abacaxi", function (req, res) {
+    res.render("pages/abacaxi")
+   });
+
+   router.get("/alface", function (req, res) {
+    res.render("pages/alface")
+   });
+
+   router.get("/alfacef", function (req, res) {
+    res.render("pages/alfacef")
+   });
+
+   router.get("/abacaxif", function (req, res) {
+    res.render("pages/abacaxif")
+   });
+
+   router.get("/ameixaf", function (req, res) {
+    res.render("pages/ameixaf")
+   });
+
+   router.get("/brocolisf", function (req, res) {
+    res.render("pages/brocolisf")
+   });
+
+   router.get("/carrinho", function (req, res) {
+    res.render("pages/carrinho")
+   });
+
+   router.get("/cadastro2", function (req, res) {
+    res.render("pages/cadastro2")
+   });
+
+   router.get("/finalizar", function (req, res) {
+    res.render("pages/finalizar")
+   });
 
 // router.post("???", function (req, res) {
 
